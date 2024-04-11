27318dec3923b7f63136b34cf308e13ec741c770
Raquel <raquel.silva@contractor-mc1.com>
2024-04-11 14:23:07 -0300

    initial commit and adjusts for project in Java

---
+++ app/src/main/java/com/example/imagepickerjava/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.imagepickerjava;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+    ActivityResultLauncher<Intent> galleryActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    Uri image_uri = result.getData().getData();
+                    imageView.setImageURI(image_uri);
+                }
+            });
+    ImageView imageView;
+    Button galleryBtn, cameraBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        galleryBtn = findViewById(R.id.galleryBtn);
+        cameraBtn = findViewById(R.id.cameraBtn);
+
+        setupListeners();
+    }
+
+    private void setupListeners() {
+        galleryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                galleryActivityResultLauncher.launch(galleryIntent);
+            }
+        });
+
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
+
